4fa5a3bddf1a6581a07ebd9a9b1adb8028c669de
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-08-14 18:50:37 +0900

    jsonschema2md が出力する見出し記号 (#) をより深いレベルにしたい場合のオプションを追記

--- documenttool/jsonschema_to_md.py
+++ documenttool/jsonschema_to_md.py
@@ -5,7 +5,7 @@ import json
 import jsonschema2md
 import pyperclip
 
-def json_schema_to_markdown(schema_file_path: str) -> str:
+def json_schema_to_markdown(schema_file_path: str, add_paragraph_count: int = 0) -> str:
     parser = jsonschema2md.Parser(
         examples_as_yaml=False,
         show_examples="all"
@@ -13,6 +13,8 @@ def json_schema_to_markdown(schema_file_path: str) -> str:
 
     with open(schema_file_path, "r") as f:
         markdown_lines = parser.parse_schema(json.load(f))
+        if add_paragraph_count > 0:
+            markdown_lines = [("#" * add_paragraph_count) + line if line.startswith('#') else line for line in markdown_lines]
 
     return ''.join(markdown_lines)
 
@@ -20,10 +22,15 @@ if __name__ == "__main__":
 
     if len(sys.argv) < 2:
         print("Copy a converted markdown to clipboard.")
-        print(f"Usage: python {os.path.basename(__file__)} <schema_file_path>")
+        print(f"Usage: python {os.path.basename(__file__)} <schema_file_path> [add paragraph ('#') count]")
         sys.exit(1)
 
-    schema_file_path = sys.argv[1]
-    md_txt = json_schema_to_markdown(schema_file_path)
+    args = sys.argv[1:]
+
+    schema_file_path = args[0]
+    add_paragraph_count = int(args[1]) if len(args) > 1 else 0
+
+    md_txt = json_schema_to_markdown(schema_file_path, add_paragraph_count)
     pyperclip.copy(md_txt)
+
     print("Copied to clipboard.")
