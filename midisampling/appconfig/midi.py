bbca1529c32561bd954bd205aad1568183c5dec1
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2024-08-16 22:20:20 +0900

    MIDI Note と Velocity に個別の数値以外に範囲指定による設定も可能にした
    記述量の削減

--- midisampling/appconfig/midi.py
+++ midisampling/appconfig/midi.py
@@ -8,6 +8,34 @@ THIS_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(THIS_SCRIPT_DIR, "midi-config.schema.json"), "r") as f:
     config_json_schema = json.load(f)
 
+def _parse_midi_byte_range(json_body: dict) -> List[int]:
+    """
+    Parse MIDI byte range from JSON body to list of int
+    Parameters
+    ----------
+    json_body : dict
+        JSON body to parse (schema:midi-byte-range.schema.json)
+
+    Returns
+    -------
+    List[int]
+        List of int values of MIDI byte range
+    """
+    result: List[int] = []
+
+    for data in json_body:
+        # from - to value
+        if type(data) == dict:
+            for i in range(data["from"], data["to"]+1):
+                result.append(i)
+        # single integer
+        elif type(data) == int:
+            result.append(data)
+        else:
+            raise ValueError(f"Invalid data format (={json_body})")
+
+    return result
+
 class MidiConfig:
 
     class ProgramChange:
@@ -23,14 +51,16 @@ class MidiConfig:
         self.pre_send_smf_path_list: List[str]                      = config_json["pre_send_smf_path_list"]
         self.midi_channel: int                                      = config_json["midi_channel"]
         self.program_change_list: List[MidiConfig.ProgramChange]    = []
-        self.midi_notes: List[int]                                  = config_json["midi_notes"]
-        self.midi_velocities: List[int]                             = config_json["midi_velocities"]
+        self.midi_notes: List[int]                                  = _parse_midi_byte_range(config_json["midi_notes"])
+        self.midi_velocities: List[int]                             = _parse_midi_byte_range(config_json["midi_velocities"])
         self.midi_pre_wait_duration: float                          = config_json["midi_pre_wait_duration"]
         self.midi_note_duration: int                                = config_json["midi_note_duration"]
         self.midi_release_duration: float                           = config_json["midi_release_duration"]
 
         for pc in config_json["midi_program_change_list"]:
             self.program_change_list.append(MidiConfig.ProgramChange(pc))
+
+
 
 def load(config_path: str) -> MidiConfig:
     with open(config_path, "r") as f:
