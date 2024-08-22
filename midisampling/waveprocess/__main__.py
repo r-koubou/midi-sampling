6c13530179cc0347912bd2254d3f7d0e1c5fb947
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2024-08-23 00:40:50 +0900

    出力ファイルパス操作を差し替え

--- midisampling/waveprocess/__main__.py
+++ midisampling/waveprocess/__main__.py
@@ -19,6 +19,7 @@ def main() -> None:
     normalize_parser.add_argument("input_directory", help="Path to the input directory with audio files (*.wav).")
     normalize_parser.add_argument("output_directory", help="Path to the output directory to save the normalized audio files.")
     normalize_parser.add_argument("target_db", type=float, help="Target decibel level for normalization.")
+    normalize_parser.add_argument("-o", "--overwrite", help="Overwrite the original audio files.", action="store_true")
 
     # Trim
     trim_parser = subparsers.add_parser("trim")
@@ -26,6 +27,7 @@ def main() -> None:
     trim_parser.add_argument("output_directory", help="Path to the output directory to save the trimmed audio files.")
     trim_parser.add_argument("threshold", type=float, help="Threshold for trimming silence. (db)")
     trim_parser.add_argument("min_silence", type=int, help="Minimum silence duration to be trimmed. (msec)")
+    trim_parser.add_argument("-o", "--overwrite", help="Overwrite the original audio files.", action="store_true")
 
 
     args = parser.parse_args()
@@ -36,17 +38,19 @@ def main() -> None:
         # Process
 
         if args.command == "normalize":
-            normalize.normalize_across_mitiple(
+            normalize.normalize_from_directory(
                 input_directory=args.input_directory,
                 output_directory=args.output_directory,
-                target_peak_dBFS=args.target_db
+                target_peak_dBFS=args.target_db,
+                overwrite=args.overwrite
             )
         elif args.command == "trim":
-            trim.batch_trim(
+            trim.trim_from_directory(
                 input_directory=args.input_directory,
                 output_directory=args.output_directory,
                 threshold_dBFS=args.threshold,
-                min_silence_ms=args.min_silence
+                min_silence_ms=args.min_silence,
+                overwrite=args.overwrite
             )
     except Exception as e:
         print(e)
--- midisampling/waveprocess/trim.py
+++ midisampling/waveprocess/trim.py
@@ -1,10 +1,14 @@
+from typing import List
+
 import os
-import sys
+import pathlib
 import shutil
 from logging import getLogger
 
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
+
+from midisampling.exportpath import RecordedAudioPath, PostProcessedAudioPath
 
 logger = getLogger(__name__)
 
@@ -30,8 +34,6 @@ def trim(input_path:str, output_path: str, threshold_dBFS:float=-50, min_silence
     audio = AudioSegment.from_wav(input_path)
     nonsilent_ranges = detect_nonsilent(audio, min_silence_len=min_silence_ms, silence_thresh=threshold_dBFS)
 
-    result_message = f"input={input_path}, output={output_path}, threshold_dBFS={threshold_dBFS}, min_silence_ms={min_silence_ms}"
-
     if nonsilent_ranges:
         start, end = nonsilent_ranges[0][0], nonsilent_ranges[-1][1]
         trimmed_audio = audio[start:end]
@@ -41,7 +43,37 @@ def trim(input_path:str, output_path: str, threshold_dBFS:float=-50, min_silence
             shutil.copy(input_path, output_path)
 
 
-def batch_trim(input_directory: str, output_directory: str, threshold_dBFS:float=-50, min_silence_ms:int=250):
+def trim_from_list(file_list: List[PostProcessedAudioPath], threshold_dBFS:float=-50, min_silence_ms:int=250):
+    """
+    Trim silent segments from all audio files in the input directory.
+
+    Parameters
+    ----------
+    file_list : List[PostProcessedAudioPath]
+        List of PostProcessedAudioPath instances.
+
+    threshold_dBFS : float (default=-50)
+        Silence threshold in dBFS.
+
+    min_silence_ms : int (default=250)
+        Minimum silence duration in milliseconds.
+    """
+
+    if file_list is None:
+        raise ValueError("file_list is None")
+    if len(file_list) == 0:
+        raise ValueError("file_list is empty")
+
+    for file in file_list:
+        input_path  = file.recorded_audio_path.path()
+        output_path = file.path()
+        file.makedirs()
+        trim(input_path, output_path, threshold_dBFS, min_silence_ms)
+        logger.info(f"Trimmed: {file.path()}")
+
+    logger.info(f"Trim End")
+
+def trim_from_directory(input_directory: str, output_directory: str, threshold_dBFS:float=-50, min_silence_ms:int=250, overwrite: bool = False):
     """
     Trim silent segments from all audio files in the input directory.
 
@@ -60,16 +92,9 @@ def batch_trim(input_directory: str, output_directory: str, threshold_dBFS:float
         Minimum silence duration in milliseconds.
     """
 
-    os.makedirs(output_directory, exist_ok=True)
-
-    logger.info("Trim Begin")
-    logger.debug(f"Trim Begin: {locals()}")
-
-    for filename in os.listdir(input_directory):
-        if filename.endswith('.wav'):
-            input_path = os.path.join(input_directory, filename)
-            output_path = os.path.join(output_directory, filename)
-            trim(input_path, output_path, threshold_dBFS, min_silence_ms)
-            logger.info(f"Trimmed: {filename}")
-
-    logger.info(f"Trim End")
+    process_files: List[PostProcessedAudioPath] = PostProcessedAudioPath.from_directory(
+        input_directory=input_directory,
+        output_directory=output_directory,
+        overwrite=overwrite
+    )
+    trim_from_list(process_files, threshold_dBFS, min_silence_ms)
