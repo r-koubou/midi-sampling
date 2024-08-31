558bde326b328341faa8329c4b6826d47e45b9ec
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2024-08-31 22:06:49 +0900

    波形処理アプリケーション画のログ出力制御を実装

--- midisampling/waveprocess/__main__.py
+++ midisampling/waveprocess/__main__.py
@@ -1,7 +1,10 @@
 from typing import List
+import os.path
 import sys
 import traceback
 import argparse
+import datetime
+from logging import getLogger
 
 from midisampling.waveprocess import normalize
 from midisampling.waveprocess import trim
@@ -11,19 +14,40 @@ from midisampling.appconfig.audioprocess import AudioProcessConfig
 from midisampling.waveprocess.processing import process
 from midisampling.waveprocess.processing import validate_effect_config
 
-from midisampling.logging_management import init_logging_as_stdout
+from midisampling.logging_management import init_logging_from_config, OutputMode
+
+logger = getLogger(__name__)
 
 def main() -> None:
 
     parser = argparse.ArgumentParser(prog=f"python -m {__package__}")
-    parser.add_argument("-v", "--verbose", help="Enable verbose logging.", action="store_true")
     parser.add_argument("processing_config_path", help="Path to the processing configuration file.")
     parser.add_argument("input_directory", help="Path to the input directory with audio files (*.wav).")
     parser.add_argument("output_directory", help="Path to the output directory to save the processed audio files.")
+    parser.add_argument("-l", "--log-file", help="Path to save the log file.")
+
+    log_level_group = parser.add_mutually_exclusive_group()
+    log_level_group.add_argument("-v", "--verbose", help="Enable verbose logging.", action="store_true")
+    log_level_group.add_argument("-q", "--quiet", help="Quiet mode. Only error messages are output.", action="store_true")
 
     args = parser.parse_args()
 
-    init_logging_as_stdout(args.verbose)
+    #----------------------------------------------------------------
+    # Initialize logging
+    #----------------------------------------------------------------
+    logfile_path = args.log_file
+    if not logfile_path:
+        timestamp    = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        logfile_dir  = os.path.dirname(os.path.abspath(args.processing_config_path))
+        logfile_path = os.path.join(logfile_dir, f"wave-process-{timestamp}.log")
+
+    output_mode = OutputMode.Default
+    if args.verbose:
+        output_mode = OutputMode.Verbose
+    elif args.quiet:
+        output_mode = OutputMode.Quiet
+
+    init_logging_from_config(logfile_path=logfile_path, output_mode=output_mode)
 
     try:
         process_config = AudioProcessConfig(args.processing_config_path)
