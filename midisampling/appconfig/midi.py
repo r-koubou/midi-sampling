8e57d55df4041b55be33a2b6dd76051ce6485cfe
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2024-09-02 18:14:31 +0900

    sample_zone_complex を外部化

--- midisampling/appconfig/midi.py
+++ midisampling/appconfig/midi.py
@@ -25,6 +25,7 @@ validator: JsonValidator = JsonValidator(
         ("midi-message-byte-range.schema.json", os.path.join(SCHEMA_FILES_DIR, "midi-message-byte-range.schema.json")),
         ("midi-velocity-layer.schema.json", os.path.join(SCHEMA_FILES_DIR, "midi-velocity-layer.schema.json")),
         ("midi-velocity-layer-preset.schema.json", os.path.join(SCHEMA_FILES_DIR, "midi-velocity-layer-preset.schema.json")),
+        ("sample-zone-complex.schema.json", os.path.join(SCHEMA_FILES_DIR, "sample-zone-complex.schema.json")),
     ])
 )
 
