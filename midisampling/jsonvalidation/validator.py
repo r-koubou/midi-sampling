edee3e5b9882f711606751af0bd2c2fe27b7c4e5
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2024-09-02 15:54:40 +0900

    registerの操作ミス修正

--- midisampling/jsonvalidation/validator.py
+++ midisampling/jsonvalidation/validator.py
@@ -3,26 +3,25 @@ import os
 import json
 from logging import getLogger
 from jsonschema import validate as json_validate
-from referencing import Registry
+from referencing import Registry, Resource
 
 logger = getLogger(__name__)
 
 class JsonSchemaInfo:
-    def __init__(self, schema_uri: str, schema_file_path: str):
-        """
-        Initialize the JsonSchemaInfo with a schema URI and a schema file path.
 
-        Parameters
-        ----------
-        schema_uri: str
-            The URI of the schema. This value is used in the "$ref" field of the schema.
-        schema_file_path: str
-            The file path of the schema.
-        """
-        self.schema_uri = schema_uri
-        self.schema_file_path = schema_file_path
+    @classmethod
+    def from_content(cls, schema_uri: str, schema_content: any):
+        return cls(schema_uri, schema_content)
+
+    @classmethod
+    def from_file(cls, schema_uri: str, schema_file_path: str):
         with open(schema_file_path, "r", encoding="utf-8") as f:
-            self.schema = json.load(f)
+            schema = json.load(f)
+        return cls(schema_uri, schema)
+
+    def __init__(self, schema_uri: str, schema: any):
+        self.schema_uri: str = schema_uri
+        self.schema: any = schema
 
 class JsonValidator:
     def __init__(self, main_schema: JsonSchemaInfo, sub_schema_info_list: List[JsonSchemaInfo] = []):
@@ -88,39 +87,18 @@ class JsonValidator:
         """
         self.main_schema = main_schema
         self.sub_schema_info_list = sub_schema_info_list
-        self.registry = Registry().with_resource(main_schema.schema_uri, main_schema.schema)
+        self.registry = Registry().with_resources([
+            (main_schema.schema_uri, Resource.from_contents(main_schema.schema))
+        ])
 
         for x in self.sub_schema_info_list:
-            self.registry = self.registry.with_resource(x.schema_uri, x.schema)
+            self.registry = self.registry.with_resources([
+                (x.schema_uri, Resource.from_contents(x.schema))
+            ])
 
-        print(self.registry.items())
-
-        for uri, _ in self.registry.items():
-            print(f"  - {uri}")
-
-    def validate(self, json_body: dict) -> bool:
+    def validate(self, json_body) -> bool:
         json_validate(
             instance=json_body,
             schema=self.main_schema.schema,
             registry=self.registry
         )
-
-
-if __name__ == "__main__":
-    import sys
-    THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-
-    try:
-        main_schema = JsonSchemaInfo(
-            schema_uri="main",
-            schema_file_path=os.path.join(THIS_DIR, "..", "appconfig", "midi-config.schema.json")
-        )
-
-        with open(sys.argv[1], "r", encoding="utf-8") as f:
-            json_body = json.load(f)
-
-        validator = JsonValidator(main_schema)
-        validator.validate(json_body)
-        print("OK")
-    except Exception as e:
-        print(e)
