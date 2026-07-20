596c406be5abaed3cd6111915ac338f1e7b1c9df
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2026-07-20 22:06:23 +0900

    デモスクリプトの移動

---
+++ src/midi_sampling/devices/demo/audio_demo.py
@@ -0,0 +1,43 @@
+import typer
+import os
+import os.path
+
+from logging import getLogger
+
+from midi_sampling.devices.audio.abstractions import AudioDevice
+from midi_sampling.devices.audio.sounddevice_impl import SdAudioDevice
+
+from midi_sampling.logging_management import init_logging_as_stdout
+
+logger = getLogger(__name__)
+
+def main(audio_information_file_path: str):
+
+    init_logging_as_stdout()
+
+    audio_device: AudioDevice = SdAudioDevice(audio_information_file_path)
+
+    logger.info("=" * 80)
+    logger.info("Available audio devices:")
+    logger.info("=" * 80)
+    for name in audio_device.get_device_names():
+        logger.info(name)
+    logger.info("=" * 80)
+
+    audio_device.initialize()
+
+    try:
+        duration = 2
+        audio_device.start_recording(duration)
+        audio_device.stop_recording()
+
+        output_file_path = ".temp/output.wav"
+        os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+
+        audio_device.export_audio(output_file_path)
+        logger.info(f"Audio exported to {output_file_path}")
+    finally:
+        audio_device.dispose()
+
+if __name__ == "__main__":
+    typer.run(main)
