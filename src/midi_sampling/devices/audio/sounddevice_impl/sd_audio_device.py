a4c305fbcf28a5b168a060d6b58504c8690ab80c
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2026-07-20 21:23:58 +0900

    サンプルコード整理

--- src/midi_sampling/devices/audio/sounddevice_impl/sd_audio_device.py
+++ src/midi_sampling/devices/audio/sounddevice_impl/sd_audio_device.py
@@ -2,6 +2,7 @@ import os
 
 # Enable ASIO support if available
 os.environ["SD_ENABLE_ASIO"] = "1"
+print("*"*100)
 
 import logging
 import numpy as np
@@ -145,40 +146,3 @@ class SdAudioDevice(AudioDevice):
             samplerate=info.sample_rate,
             subtype=sub_type,
         )
-
-
-if __name__ == "__main__":
-    import typer
-    import os
-    import os.path
-    from midi_sampling.logging_management import init_logging_as_stdout
-
-    def main(audio_information_file_path: str):
-
-        init_logging_as_stdout()
-
-        audio_device: AudioDevice = SdAudioDevice(audio_information_file_path)
-
-        logger.info("=" * 80)
-        logger.info("Available audio devices:")
-        logger.info("=" * 80)
-        for name in audio_device.get_device_names():
-            logger.info(name)
-        logger.info("=" * 80)
-
-        audio_device.initialize()
-
-        try:
-            duration = 2
-            audio_device.start_recording(duration)
-            audio_device.stop_recording()
-
-            output_file_path = ".temp/output.wav"
-            os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
-
-            audio_device.export_audio(output_file_path)
-            logger.info(f"Audio exported to {output_file_path}")
-        finally:
-            audio_device.dispose()
-
-    typer.run(main)
